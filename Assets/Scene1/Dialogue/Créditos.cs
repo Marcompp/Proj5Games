4006e86d3ac2031b3c1a6902467dab4a40731a5b
Eric Fernando Otofuji Abrantes <ericfoa@al.insper.edu.br>
2022-06-01 05:04:21 -0300

    Sprint 2

---
+++ Assets/Scene1/Dialogue/Créditos.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Créditos : MonoBehaviour
+{
+    // Cena de diálogo baseada no tutorial de https://youtu.be/_nRzoTzeyxU
+    // Código aberto licenciado de https://github.com/Brackeys/Dialogue-System.git
+}
