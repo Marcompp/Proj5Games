be957e18130aed2a13a35e43ea77e4a0cf49a720
José Hélio Paiva Neto <hpaivan@gmail.com>
2022-06-10 15:18:19 -0300

    up

--- Assets/Shared/_Script/EnemysController.cs
+++ Assets/Shared/_Script/EnemysController.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.U2D;
-using UnityEngine.SceneManagement;
-using Random = UnityEngine.Random;
 
 public class EnemysController : MonoBehaviour
 {
